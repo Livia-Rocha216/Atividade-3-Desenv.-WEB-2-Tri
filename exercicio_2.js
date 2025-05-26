6b418b74fbdee7184846e752927d1367564d47f8
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-05-26 23:30:53 +0000

    exercicio 2 feito

--- exercicio_2.js
+++ exercicio_2.js
@@ -1,12 +1,16 @@
 let peso = 60;
 let altura = 1.80;
+let imc = peso / (altura * altura)
 
-imc = peso / (altura * altura)
-
-if(imc <= 18.5){
-
-}else if()
-
-// meu imc: 18.52
-
-if 
+if(imc < 18.5){
+    console.log("IMC = ", imc, "- Abaixo do peso.")
+}
+else if(18.5 <= imc < 25){
+    console.log("IMC = ", imc, "- Peso normal.")
+}
+else if(25 <= imc < 30){
+    console.log("IMC = ", imc, "- Sobrepeso.")
+}
+else if(imc >= 30){
+    console.log("IMC = ", imc, "- Obesidade.")
+}
