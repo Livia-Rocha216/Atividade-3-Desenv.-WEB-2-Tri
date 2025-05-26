abb626fb13de612eaf03dbc2c48aefca702fcf49
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-05-26 23:37:28 +0000

    codigos explicados

--- exercicio_1.js
+++ exercicio_1.js
@@ -1,14 +1,15 @@
-let idade = 15;
-let nome = "Lívia";
+let idade = 15; // Variável de idade;
+let nome = "Lívia"; // Variável de nome;
 
-function tempo_vivido(){
-    console.log("----------------------")
+function tempo_vivido(){ // Eu criei uma função por achar mais fácil executar o bloco de código inteiro com apenas um comando;
+                         // a função pega os valores de idade e faz suas devidas multiplicações pra obter os valores desejados.
+    console.log("----------------------") // Decoração :D
     console.log("Nome:", nome)
     console.log("Idade:", idade)
     console.log("Dias vividos:", idade * 365)
     console.log("Minutos vividos:", idade * 525960)
     console.log("Segundos vividos:", idade * 31560000)
-    console.log("----------------------")
+    console.log("----------------------") // Decoração :D
 }
 
-tempo_vivido()
+tempo_vivido() // Puxar a função aqui executa o bloco de código anterior todo.
--- exercicio_2.js
+++ exercicio_2.js
@@ -1,16 +1,16 @@
-let peso = 60;
-let altura = 1.80;
-let imc = peso / (altura * altura)
+let peso = 60; // Variável de peso;
+let altura = 1.80; // Variável de altura;
+let imc = peso / (altura * altura) // Variável IMC que faz o cálculo pra descobrir o IMC uau
 
-if(imc < 18.5){
+if(imc < 18.5){ // SE o imc for menor que 18.5, ele mostra a mensagem de estar abaixo do peso.
     console.log("IMC = ", imc, "- Abaixo do peso.")
 }
-else if(18.5 <= imc < 25){
+else if(18.5 <= imc < 25){ // SE o imc for menor que 25 mas maior ou igual a 18.5, mostra a mensagem de peso normal.
     console.log("IMC = ", imc, "- Peso normal.")
 }
-else if(25 <= imc < 30){
+else if(25 <= imc < 30){ // SE o imc for menor que 30 mas maior ou igual a 25, mostra a mensagem de sobrepeso.
     console.log("IMC = ", imc, "- Sobrepeso.")
 }
-else if(imc >= 30){
+else if(imc >= 30){ // e por último, SE o imc for maior ou igual a 30, mostra a mensagem de obesidade.
     console.log("IMC = ", imc, "- Obesidade.")
 }
