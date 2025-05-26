28b1c0803ecd1a826034a6c109e217a1673c7c44
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-05-26 16:22:59 +0000

    TERMINAR EM CASA

---
+++ exercicio_1.js
@@ -0,0 +1,14 @@
+let idade = 15;
+let nome = "Lívia";
+
+function tempo_vivido(){
+    console.log("----------------------")
+    console.log("Nome:", nome)
+    console.log("Idade:", idade)
+    console.log("Dias vividos:", idade * 365)
+    console.log("Minutos vividos:", idade * 525960)
+    console.log("Segundos vividos:", idade * 31560000)
+    console.log("----------------------")
+}
+
+tempo_vivido()
---
+++ exercicio_2.js
@@ -0,0 +1,6 @@
+let peso = 60;
+let altura = 1.80;
+
+// meu imc: 18.52
+
+if 
